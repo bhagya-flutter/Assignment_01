4db6172b362882d5e8b0b4b7e9d93ea5d427cad5
“bhagya-flutter” <bhagya.c2wflutter@gmail.com>
2024-02-15 20:20:01 +0530

    pushed assignment_01

--- assignment_01/lib/main.dart
+++ assignment_01/lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Assignment 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Core2Web Technologies',
+              textAlign: TextAlign.center,
+                   style: TextStyle(fontSize: 35, color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
